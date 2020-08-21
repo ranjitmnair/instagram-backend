06705e16b86138a05bf02da10221705d35466c63
ranjitmnair <ranjitnair19@gmail.com>
2020-08-21 12:39:58 +0530

    like/dislike in one route

--- routes/posts.js
+++ routes/posts.js
@@ -1,85 +1,107 @@
-const router=require('express').Router();
-const Post=require('../models/Post');
-const bcrypt=require('bcryptjs');
-const jwt=require('jsonwebtoken');
-const verifyToken=require('../middleware/verifyToken');
-const {postValidation}=require('../validation');
+const router = require ('express').Router ();
+const Post = require ('../models/Post');
+const bcrypt = require ('bcryptjs');
+const jwt = require ('jsonwebtoken');
+const verifyToken = require ('../middleware/verifyToken');
+const {postValidation} = require ('../validation');
 
-router.get('/getposts',verifyToken,async(req,res)=>{
-    
-    try {
-        const posts=await Post
-        .find()
-        .populate("postedBy","_id name");
+router.get ('/getposts', verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find ().populate ('postedBy', '_id name');
 
-        return res.status(200).json({posts});
-    } catch (error) {
-        return res.status(400).json({error})
+    return res.status (200).json ({posts});
+  } catch (error) {
+    return res.status (400).json ({error});
+  }
+});
+
+router.post ('/createpost', verifyToken, async (req, res) => {
+  // validation
+  const {error} = postValidation (req.body);
+  if (error) return res.status (400).json (error.details[0].message);
+
+  req.user.password = null;
+  const post = new Post ({
+    title: req.body.title,
+    body: req.body.body,
+    postedBy: req.user,
+  });
+  try {
+    const savedPost = await post.save ();
+    return res.status (200).json (post);
+  } catch (err) {
+    return res.status (400).json ({err});
+  }
+});
+
+router.get ('/myposts', verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find ({postedBy: req.user._id}).populate (
+      'postedBy',
+      '_id name'
+    );
+    return res.status (200).json ({posts});
+  } catch (error) {
+    return res.status (401).send ({error});
+  }
+});
+
+//like dislike  route
+router.put ('/like', verifyToken, async (req, res) => {
+  const currentPost = await Post.findById (req.body.postId);
+  for(var i=0;i<currentPost.likes.length;i++){
+      if(String(currentPost.likes[i])==String(req.user._id)){
+  
+         console.log(currentPost.likes[i]);
+     const post = await Post.findByIdAndUpdate (
+        req.body.postId,
+        {
+          $pull: {likes: req.user._id},
+          $inc: {likeCount: -1},
+        },
+        {
+          new: true,
+        }
+      );
+      try {
+        return res.status (200).json (post);
+      } catch (error) {
+        return res.status (400).json ({error});
+      }
     }
-})
+}   
 
-
-
-router.post('/createpost',verifyToken,async(req,res)=>{
-    // validation
-    const {error}=postValidation(req.body);
-    if(error)return res.status(400).json(error.details[0].message);
-
-    req.user.password=null;
-    const post=new Post({
-        title:req.body.title,
-        body:req.body.body,
-        postedBy:req.user
-    });
-    try{
-        const savedPost= await post.save();
-        return res.status(200).json(post);
-    }catch(err){
-        return res.status(400).json({err});
+  const post = await Post.findByIdAndUpdate (
+    req.body.postId,
+    {
+      $push: {likes: req.user._id},
+      $inc: {likeCount: 1},
+    },
+    {
+      new: true,
     }
+  );
 
-})
-
-router.get('/myposts',verifyToken,async(req,res)=>{
-    try {
-        const posts=await Post.find({postedBy:req.user._id})
-        .populate("postedBy","_id name");
-         return res.status(200).json({posts});
-    } catch (error) {
-        return res.status(401).send({error});
-    }
-})
-
-//like route
-router.put('/like',verifyToken,async(req,res)=>{
-    const post=await Post.findByIdAndUpdate(req.body.postId,{
-        $push:{likes:req.user._id},
-        $inc: { likeCount: 1 } 
-    },{
-        new:true
-    })
-    
-    try {
-        return res.status(200).json(post)
-    } catch (error) {
-        return res.status(400).json({error});
-    }
-})
+  try {
+    return res.status (200).json (post);
+  } catch (error) {
+    return res.status (400).json ({error});
+  }
+});
 
 //unlike feature
-router.put('/unlike',verifyToken,async(req,res)=>{
-    const post=await Post.findByIdAndUpdate(req.body.postId,{
-        $pull:{likes:req.user._id},
-        $inc: { likeCount: -1 } 
-    },{
-        new:true
-    })
-    try {
-        return res.status(200).json(post)
-    } catch (error) {
-        return res.status(400).json({error});
-    }
-})
+// router.put('/unlike',verifyToken,async(req,res)=>{
+//     const post=await Post.findByIdAndUpdate(req.body.postId,{
+//         $pull:{likes:req.user._id},
+//         $inc: { likeCount: -1 }
+//     },{
+//         new:true
+//     })
+//     try {
+//         return res.status(200).json(post)
+//     } catch (error) {
+//         return res.status(400).json({error});
+//     }
+// })
 
-
-module.exports=router;
+module.exports = router;
